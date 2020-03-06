7deec959d034faa6b67a3bb06bce9bf5f5d780a5
yun-mh <minhoyun.dev@gmail.com>
2020-03-06 17:10:54 +0900

    Implement Search

--- README.md
+++ README.md
@@ -8,3 +8,14 @@ Learning React and ES6 by building a Movie discover app.
 - [ ] TV Shows
 - [ ] Search
 - [ ] Detail
+
+## API Verbs
+
+- [x] Now playing (Movie)
+- [x] Upcoming (Movie)
+- [x] Top Rated (TV)
+- [x] Popular (Movie, TV)
+- [x] Airing Today (TV)
+- [x] TV show detail
+- [x] Movie detail
+- [x] Search movies and tvs
---
+++ src/Routes/Search/SearchContainer.js
@@ -0,0 +1,51 @@
+import React from "react";
+import SearchPresenter from "./SearchPresenter";
+import { moviesApi, tvApi } from "../../api";
+
+export default class extends React.Component {
+  state = {
+    movieResults: null,
+    tvResults: null,
+    searchTerm: "code",
+    loading: false,
+    error: null
+  };
+  
+  componentDidMount() {
+    this.handleSubmit();
+  }
+
+  handleSubmit = () => {
+    const { searchTerm } = this.state;
+    if (searchTerm !== "") {
+      this.searchByTerm();
+    }
+  };
+
+  searchByTerm = async () => {
+    const { searchTerm } = this.state;
+    try {
+      const {data: { results: movieResults }} = await moviesApi.search(searchTerm);
+      const {data: { results: tvResults }} = await tvApi.search(searchTerm);
+      this.setState({ movieResults: movieResults, tvResults: tvResults, loading: true });
+    } catch {
+      this.setState({ error: "Cannot find results." });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  render() {
+    const { movieResults, tvResults, searchTerm, loading, error } = this.state;
+    console.log(this.state)
+    return (
+      <SearchPresenter
+        movieResults={movieResults}
+        tvResults={tvResults}
+        searchTerm={searchTerm}
+        loading={loading}
+        error={error}
+      />
+    );
+  }
+}
