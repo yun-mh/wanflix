a805f3e718e144987a10922b0e351d18563f1c6a
yun-mh <minhoyun.dev@gmail.com>
2020-03-09 16:16:14 +0900

    Updates

---
+++ precache-manifest.976b34419b2a9a9c475797d9a9c4e92d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12eb72fa581cfb414a25cf35ccff7a5c",
+    "url": "/wanflix/index.html"
+  },
+  {
+    "revision": "be556ebada222c492818",
+    "url": "/wanflix/static/js/2.d930237b.chunk.js"
+  },
+  {
+    "revision": "280b48dbc3281f342da16f66e41cf46c",
+    "url": "/wanflix/static/js/2.d930237b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "659002506f97303eb4bb",
+    "url": "/wanflix/static/js/main.baf6e23e.chunk.js"
+  },
+  {
+    "revision": "2cf18e033eac65f67bc1",
+    "url": "/wanflix/static/js/runtime-main.1e376061.js"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/wanflix/static/media/noPosterSmall.87a0eb6a.png"
+  }
+]);
